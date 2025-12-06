64902de8a2cc2bd3ef9222b1a25f73570eb97f0d
illuminatingaxolotl <svenja_wege@gmx.de>
2025-12-06 21:07:32 +0100

    warum tut d2 nicht?

--- d3/d1.py
+++ d3/d1.py
@@ -37,97 +37,53 @@ def d3_2():
    
     for bank in file:
         
+        
         bank=bank.strip("\n")
         bank=[int(x) for x in bank]
-        #print(bank)
-        #memory=[0 for x in bank]
-        
         
         
         start=len(bank)-13
-        #print(start)
         current=bank[start]
-        currentid=start
+        
         mem=0
-        
-        print()
-        
         mincount=0
+       
+        
         for i in range(start+1):
-            #print(start-i)
+
             mincount+=1
             comp=bank[start-i]
             compid=start-i
-            #print(comp)
-            
-            
+
             if comp>=current:
                 current=comp
                 
                 for i in range(mincount):
-                    #print(bank[compid:])
+ 
                     min0=min(bank[compid:])
-                   # print(min0)
+                    #(bank[compid:]).remove(min0)
+                    
                     minindex=bank[compid:].index(min0)+compid
-                   # print(minindex)
                     bank.pop(minindex)
-                   # print(bank[compid:])
+                   
        
                 mincount=0
-                #print(compid)
-                
                 mem=compid
-        
-        
-       # print(mem)
-      
-       
-      #print(bank)
+
         bank=(bank[mem:])
         print(bank)
-        #print(bank[compid:])
-        # while len(bank)>12:
-        #     bank.remove(min(bank))
-        
-        
-        
+
         joltage_line=""
         
         for num in bank:
             joltage_line+=str(num)
-            
-        #print(joltage_line)
-            
-      
+ 
         joltage+=int(joltage_line)
-    
-            
-        
 
-    
     file.close()
     return joltage
         
         
               
-        
-
-
-def getmax(l,memory):
-        
-        if len(l)==1:
-            return memory+str(l[0])
-        
-        else:
-            maxpos=l.index(max(l))
-            l[maxpos]=0
-            return getmax(l[maxpos:],memory)
-        
-        #return memory
-        
-        
-           
-          
-            
 
 print(d3_2())
